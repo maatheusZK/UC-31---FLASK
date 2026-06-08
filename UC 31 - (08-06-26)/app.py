1ac24d30828186760043701da083353443237936
maatheusZK <maatheusseveriano@gmail.com>
2026-06-08 16:19:09 -0300

    Add files via upload

---
+++ UC 31 - (08-06-26)/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    tema = request.cookies.get('tema', 'claro')
+    return render_template('index.html', tema=tema)
+
+@app.route('/tema/<escolha>')
+def trocar_tema(escolha):
+    if escolha not in ['claro', 'escuro']:
+        escolha = 'claro'
+
+    resposta = make_response(redirect(url_for('inicio')))
+    resposta.set_cookie('tema', escolha, max_age=60*60*24*30)
+
+    return resposta
+
+if __name__ == '__main__':
+    app.run(debug=True)
